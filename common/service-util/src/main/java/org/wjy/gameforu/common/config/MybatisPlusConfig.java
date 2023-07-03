6bfef6146f39e192c044fdb9c050313c8ebe2605
Jiangyan-Wang <wang_jiangyan@163.com>
2023-07-03 22:18:34 +0100

    add MybatisPlusConfig Class

---
+++ common/service-util/src/main/java/org/wjy/gameforu/common/config/MybatisPlusConfig.java
@@ -0,0 +1,36 @@
+package org.wjy.gameforu.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("org.wjy.gameforu.*.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    /**
+     * Mybatis-Plus的下一个版本中，setUseDeprecatedExecutor将会删除，
+     * 但是目前，为了避免缓存出现问题最好还是把这个方法写上，
+     * 这个报错对整体代码没有太大的影响…
+     */
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+}
