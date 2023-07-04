bf434942e4afd9500c2c40eac7e5d9efbc45e21d
Jiangyan-Wang <wang_jiangyan@163.com>
2023-07-04 13:35:37 +0100

    login acl api

---
+++ service/service-acl/src/main/java/org/wjy/gameforu/ServiceAclApplication.java
@@ -0,0 +1,12 @@
+package org.wjy.gameforu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Access Control List Module entry
+@SpringBootApplication
+public class ServiceAclApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ServiceAclApplication.class, args);
+    }
+}
---
+++ service/service-acl/src/main/java/org/wjy/gameforu/acl/controller/IndexController.java
@@ -0,0 +1,51 @@
+package org.wjy.gameforu.acl.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wjy.gameforu.common.result.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/admin/acl/index")
+public class IndexController {
+    /**
+     * admin controller
+     */
+
+    /**
+     * login
+     */
+    @PostMapping("/login")
+     public Result login(){
+        // return token
+        Map<String, String> map = new HashMap<>();
+        map.put("token","token-admin");
+        return Result.ok(map);
+    }
+
+    /**
+     * get info of users
+     */
+    @GetMapping("/info")
+    public Result info(){
+        // return name and avatar
+        Map<String, String> map = new HashMap<>();
+        map.put("name","admin");
+        map.put("avatar","jinitaimei");
+        return Result.ok(map);
+    }
+
+    /**
+     * logout
+     */
+    @PostMapping("/logout")
+    public Result logout(){
+
+        return Result.ok(null);
+    }
+
+}
