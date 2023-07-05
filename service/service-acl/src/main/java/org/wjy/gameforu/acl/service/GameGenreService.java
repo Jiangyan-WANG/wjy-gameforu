4c04b1555b8afdb3b99ce4a8210e1733d1639672
Jiangyan-Wang <wang_jiangyan@163.com>
2023-07-05 20:16:02 +0100

    debug: modify generic of GameGenreMapper

--- service/service-acl/src/main/java/org/wjy/gameforu/acl/service/GameGenreService.java
+++ service/service-acl/src/main/java/org/wjy/gameforu/acl/service/GameGenreService.java
@@ -1,7 +1,12 @@
 package org.wjy.gameforu.acl.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.wjy.gameforu.model.gameforu.Game;
 import org.wjy.gameforu.model.gameforu.GameGenre;
+import org.wjy.gameforu.vo.gameforu.GameQueryVo;
+
 
 public interface GameGenreService extends IService<GameGenre> {
 }
